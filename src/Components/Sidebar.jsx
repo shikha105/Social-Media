66c675ad0d7f28ab8ea832018cfdf111c55f825f
shikha105 <shikha105789@gmail.com>
2024-03-29 15:07:46 -0700

    resolved className and xlinkHref errors

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,27 +1,29 @@
 const Sidebar = ({ selectedTab }) => {
   return (
     <div
-      class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark sidebar"
+      className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark sidebar"
       style={{ width: "280px" }}
     >
       <a
         href="/"
-        class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none"
+        className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none"
       >
-        <svg class="bi pe-none me-2" width="40" height="32">
+        <svg className="bi pe-none me-2" width="40" height="32">
           <use xlinkHref="#bootstrap"></use>
         </svg>
-        <span class="fs-4">Sidebar</span>
+        <span className="fs-4">Sidebar</span>
       </a>
       <hr />
-      <ul class="nav nav-pills flex-column mb-auto">
-        <li class="nav-item">
+      <ul className="nav nav-pills flex-column mb-auto">
+        <li className="nav-item">
           <a
             href="#"
-            class={`nav-link text-white ${selectedTab === "Home" && `active`}`}
+            className={`nav-link text-white ${
+              selectedTab === "Home" && `active`
+            }`}
             aria-current="page"
           >
-            <svg class="bi pe-none me-2" width="16" height="16">
+            <svg className="bi pe-none me-2" width="16" height="16">
               <use xlinkHref="#home"></use>
             </svg>
             Home
@@ -30,11 +32,11 @@ const Sidebar = ({ selectedTab }) => {
         <li>
           <a
             href="#"
-            class={`nav-link text-white ${
+            className={`nav-link text-white ${
               selectedTab === "CreatePost" && `active`
             }`}
           >
-            <svg class="bi pe-none me-2" width="16" height="16">
+            <svg className="bi pe-none me-2" width="16" height="16">
               <use xlinkHref="#speedometer2"></use>
             </svg>
             Create Post
@@ -42,10 +44,10 @@ const Sidebar = ({ selectedTab }) => {
         </li>
       </ul>
       <hr />
-      <div class="dropdown">
+      <div className="dropdown">
         <a
           href="#"
-          class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
+          className="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
           data-bs-toggle="dropdown"
           aria-expanded="false"
         >
@@ -54,31 +56,31 @@ const Sidebar = ({ selectedTab }) => {
             alt=""
             width="32"
             height="32"
-            class="rounded-circle me-2"
+            className="rounded-circle me-2"
           />
           <strong>mdo</strong>
         </a>
-        <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
+        <ul className="dropdown-menu dropdown-menu-dark text-small shadow">
           <li>
-            <a class="dropdown-item" href="#">
+            <a className="dropdown-item" href="#">
               New project...
             </a>
           </li>
           <li>
-            <a class="dropdown-item" href="#">
+            <a className="dropdown-item" href="#">
               Settings
             </a>
           </li>
           <li>
-            <a class="dropdown-item" href="#">
+            <a className="dropdown-item" href="#">
               Profile
             </a>
           </li>
           <li>
-            <hr class="dropdown-divider" />
+            <hr className="dropdown-divider" />
           </li>
           <li>
-            <a class="dropdown-item" href="#">
+            <a className="dropdown-item" href="#">
               Sign out
             </a>
           </li>
